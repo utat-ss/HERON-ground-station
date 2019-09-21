8808fbcbbb6f25f08b2e0ec3e4bf782307de909f
Saad Khan <khanm199@mail.utoronto.ca>
2019-09-21 12:14:21 -0400

    Added basic scheduling functionality

--- website/app.py
+++ website/app.py
@@ -7,19 +7,39 @@ app = Flask(__name__)
 bootstrap = Bootstrap(app)
 app.config.from_object(Config)
 
-functionMap = {}
+schedules = []  # List of all schedules which will be imported
 
-with open('functions.json') as json_file:
-    functions = json.load(json_file)
+# Make list of commands that can be sent to satellite
+with open('data/commands.json') as json_file:
+    commands = json.load(json_file)  # List of all commands that the satellite needs to follow
 
+# Make entire list of functions, including commands, that can be executed by schedule interpreter
+with open('data/extraFunctions.txt') as function_file:
+    allFunctions = function_file.readlines()  # List of all extra functions that can be scheduled
+allFunctions = [line.strip() for line in allFunctions]
 
-@app.route('/', methods=['GET', 'POST', 'DELETE'])
+for command in commands:
+    allFunctions.append(command['name'])
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'GET':
-        return render_template('index.html', title='Home', functions=functions)
+        return render_template('index.html', title='Home', commands=commands)
     if request.method == 'POST':
-        data = request.form
+        data = request.form.to_dict()
         print("Got request for function " + data["id"])
+        print(data)
+        return ('', 204)
+
+
+@app.route('/scheduler', methods=['GET', 'POST'])
+def scheduler():
+    if request.method == 'GET':
+        return render_template('scheduler.html', title='Scheduler', schedules=schedules, functions=allFunctions)
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        data['schedule'] = json.loads(data['schedule'])
+        print("Recieved schedule")
         print(data)
         return ('', 204)
 
