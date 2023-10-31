4a625c11406bf7a2dc04066326211b9b69e09aa0
Swarnava Ghosh <swarnavaghosh04@gmail.com>
2023-10-30 21:29:04 -0400

    added delay

--- gnuradio/example.py
+++ gnuradio/example.py
@@ -1,4 +1,5 @@
 from esttc_interface import esttc_rx, esttc_tx
+import time
 import zmq
 
 if __name__ == '__main__':
@@ -6,6 +7,7 @@ if __name__ == '__main__':
     try:
         while True:
             esttc_tx([4,3,2,1,5])
+            time.sleep(1000)
             try:
                 print(esttc_rx())
             except zmq.error.Again:
