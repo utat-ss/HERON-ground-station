83e8a79d05637a3c0dbdf7c2c1bd4aceb76822e9
eosti <reidsox1@gmail.com>
2023-12-16 21:36:13 -0500

    add toptek control to wrapper

--- gnuradio/esttc_interface.py
+++ gnuradio/esttc_interface.py
@@ -1,9 +1,14 @@
 import pmt
 import zmq
+import time
+
+from toptek import Toptek
+
+SLEEPY_TIME = 0.1
 
 
 class ESTTCWrapper:
-    def __init__(self, ip: str, tx_port: int = 50491, rx_port: int = 50492):
+    def __init__(self, ip: str, tx_port: int = 50491, rx_port: int = 50492, ser: str = "/dev/ttyAmplifier"):
         self.ip = ip
         self.tx_port = tx_port
         self.rx_port = rx_port
@@ -15,6 +20,10 @@ class ESTTCWrapper:
         self.txsocket = self.context.socket(zmq.PUSH)
         self.txsocket.connect(f"tcp://{self.ip}:{self.tx_port}")
 
+        self.amp = Toptek(ser)
+        self.amp.pa_on()
+        self.amp.lna_on()
+
     def __del__(self):
         self.rxsocket.close()
         self.txsocket.close()
@@ -25,7 +34,10 @@ class ESTTCWrapper:
         message = pmt.init_u8vector(len(message), message)
         message = pmt.cons(pmt.PMT_NIL, message)
         message = pmt.serialize_str(message)
+        self.amp.da_on()
         self.txsocket.send(message)
+        time.sleep(SLEEPY_TIME)
+        self.amp.da_off()
 
     def tx_bytes(self, msg: bytes) -> None:
         message = pmt.init_u8vector(len(msg), msg)
