6580469b90e53d447e83c37bbe9f95d057d7cc04
Swarnava Ghosh <49722007+swarnavaghosh04@users.noreply.github.com>
2024-06-05 21:28:22 -0400

    added line numbering

--- tests/listen.py
+++ tests/listen.py
@@ -2,13 +2,16 @@ from threading import Thread
 import zmq
 from esttc_interface import ESTTCWrapper
 
-esttc = ESTTCWrapper("tcp://10.0.7.91:50491", "tcp://10.0.7.91:50492")
+esttc = ESTTCWrapper("tcp://10.0.1.165:50491", "tcp://10.0.1.165:50492")
 run = True
 
 def listen():
+    line = 0
     while run:
         try:
-            print(esttc.rx(zmq.NOBLOCK))
+            msg = esttc.rx(zmq.NOBLOCK)
+            line += 1
+            print('[{}] {}'.format(line, msg))
         except zmq.ZMQError:
             pass
     
