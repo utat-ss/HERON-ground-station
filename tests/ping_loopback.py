65d0a6b44b4b7d642aab5ba7efc29ed53bf91673
Swarnava Ghosh <49722007+swarnavaghosh04@users.noreply.github.com>
2024-09-15 15:55:57 -0400

    pings send out when previous one is received, and code seperated into loopback and non-loopback versions

--- tests/ping_loopback.py
+++ tests/ping_loopback.py
@@ -3,30 +3,20 @@ from threading import Thread
 import zmq
 from esttc_interface import ESTTCWrapper
 
-ping_delay = 3
+ping_delay = 1
 ping_msg = "ES+R2200\r"
 
 pings_sent = 0
 pings_rcvd = 0
 run = True
-txer = ESTTCWrapper("tcp://10.0.7.91:50491", "tcp://10.0.7.91:50492")
-rxer = ESTTCWrapper("tcp://10.0.1.165:50491", "tcp://10.0.1.165:50492")
+ping_esttc = ESTTCWrapper("tcp://10.0.1.165:50491", "tcp://10.0.1.165:50492")
 
-def txer_rx_sink():
-    recv_flush = 100000
-    while run or recv_flush>0:
-        try:
-            txer.rx(zmq.NOBLOCK)
-        except zmq.ZMQError:
-            pass
-        recv_flush -= 1-run
-
-def rxer_rx_sink():
+def rx_sink():
     global pings_rcvd
     recv_flush = 100000
     while run or recv_flush>0:
         try:
-            msg = rxer.rx(zmq.NOBLOCK)
+            msg = ping_esttc.rx(zmq.NOBLOCK)
             if msg == ping_msg:
                 pings_rcvd += 1
                 print("ping received [{}]".format(pings_rcvd))
@@ -37,12 +27,12 @@ def rxer_rx_sink():
 def pinger():
     global pings_sent
     global pings_rcvd
-    rxer.set_timeout(ping_delay*1000)
+    ping_esttc.set_timeout(ping_delay*1000)
     while run:
-        txer.tx(ping_msg)
+        ping_esttc.tx(ping_msg)
         pings_sent += 1
         try:
-            msg = rxer.rx()
+            msg = ping_esttc.rx()
             if msg == ping_msg:
                 pings_rcvd += 1
                 print("ping received [{}]".format(pings_rcvd))
@@ -50,18 +40,15 @@ def pinger():
             print('timeout')
 
 if __name__ == '__main__':
-    t_txer_rx_sink = Thread(target=txer_rx_sink)
-    t_pinger = Thread(target=pinger)
 
-    t_txer_rx_sink.start()
+    t_pinger = Thread(target=pinger)
     t_pinger.start()
 
     input("Press Enter to quit...\n")
     run = False
 
     t_pinger.join()
-    rxer_rx_sink()
-    t_txer_rx_sink.join()
+    rx_sink()
 
     total_loss = "{0:.2f} %".format((pings_sent-pings_rcvd)/pings_sent*100) if pings_sent != 0 else 'N/A'
 
