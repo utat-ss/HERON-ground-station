4c763f49c7a3d0a5c9bd789fdc0736165267fe48
Swarnava Ghosh <49722007+swarnavaghosh04@users.noreply.github.com>
2024-09-19 00:37:45 -0400

    finished ax25.pkt2str and used it to setup external ping script

--- tests/ax25.py
+++ tests/ax25.py
@@ -26,4 +26,42 @@ def str2pkt(info, dest, src, r1=None, r2=None, ):
 
 
 def pkt2str(packet):
-    pass
+    dest = ''.join(chr(c>>1) for c in packet[0:6]).strip()
+    dest_ssid_o = packet[6]
+    dest_ssid = (dest_ssid_o >> 1) & 0b1111
+    packet = packet[7:]
+
+    src = ''.join(chr(c>>1) for c in packet[0:6]).strip()
+    src_ssid_o = packet[6]
+    src_ssid = (src_ssid_o >> 1) & 0b1111
+    packet = packet[7:]
+
+    msg = f'\033[92m{dest}-{dest_ssid}\033[0m to {src}-{src_ssid}'
+    
+    if not (src_ssid_o & 1):
+        r1 = ''.join(chr(c>>1) for c in packet[0:6]).strip()
+        r1_ssid_o = packet[6]
+        r1_ssid = (r1_ssid_o >> 1) & 0b1111
+        packet = packet[7:]
+        if r1_ssid_o & (1 << 8):
+            msg += f' via \033[4m{r1}-{r1_ssid}\033[0m'
+        else:
+            msg += f' via {r1}-{r1_ssid}'
+        if not (r1_ssid_o & 1):
+            r2 = ''.join(chr(c>>1) for c in packet[0:6]).strip()
+            r2_ssid_o = packet[6]
+            r2_ssid = (r2_ssid_o >> 1) & 0b1111
+            packet = packet[7:]
+            if r2_ssid_o & (1 << 8):
+                msg += f', \033[4m{r2}-{r2_ssid}\033[0m'
+            else:
+                msg += f', {r2}-{r2_ssid}'
+    
+    ctrl = packet[0]
+    pid = packet[1]
+    info = ''.join(chr(c) for c in packet[2:])
+    msg += ': ' + info
+
+    return msg
+
+
