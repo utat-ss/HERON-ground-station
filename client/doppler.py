946bc3c41b6b8f839373cd1718347f138acf8b2a
Swarnava Ghosh <49722007+swarnavaghosh04@users.noreply.github.com>
2024-09-27 16:44:37 -0400

    added doppler correction client

---
+++ client/doppler.py
@@ -0,0 +1,12 @@
+from xmlrpc.client import ServerProxy
+
+def main():
+    dpler = ServerProxy("http://localhost:50600")
+    dpler.load_norad(25544)
+    dpler.set_freq(437_800_000)
+    dpler.enable_correction()
+    input('Press Enter to quit')
+    dpler.disable_correction()
+
+if __name__ == "__main__":
+    main()
