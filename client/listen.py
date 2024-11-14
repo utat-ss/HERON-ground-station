efdb1e052eec718441836444ce8e4f62e5fe744a
Swarnava Ghosh <swarnavaghosh04@gmail.com>
2024-11-13 21:18:44 -0500

    argparse-ed the ip

--- client/listen.py
+++ client/listen.py
@@ -1,23 +1,34 @@
 from threading import Thread
 import zmq
 from esttc_interface import ESTTCWrapper
+import argparse
 
 ip = "10.0.1.165"
-esttc = ESTTCWrapper(f"tcp://{ip}:50491", f"tcp://{ip}:50492")
 run = True
 
-def listen():
+def listen(digi):
     line = 0
     while run:
         try:
-            msg = esttc.rx(zmq.NOBLOCK)
+            msg = digi.rx(zmq.NOBLOCK)
             line += 1
             print('[{}] {}'.format(line, msg))
         except zmq.ZMQError:
             pass
     
 if __name__ == '__main__':
-    t = Thread(target=listen)
+    parser = argparse.ArgumentParser(
+        prog='listen',
+        description='dump digital interface Rx')
+    parser.add_argument(
+        '--ip',
+        type=str,
+        default='localhost',
+        help='ip address of running transceiver')
+    args = parser.parse_args()
+
+    esttc = ESTTCWrapper(f"tcp://{args.ip}:50491", f"tcp://{args.ip}:50492")
+    t = Thread(target=listen, args=[esttc,])
     t.start()
     input("Press Enter to quit...\n")
     run = False
